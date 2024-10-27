397bf2f7c70fac4d8bc4f1b237c674e83fb63ace
Jomar Milan <jomarm@jomarm.com>
2024-10-27 11:00:04 -0700

    Use astronvim

--- submodule-remotes.sh
+++ submodule-remotes.sh
@@ -5,9 +5,3 @@ cd hyprland-conf
 if ! git config remote.upstream.url > /dev/null; then
     git remote add upstream https://github.com/AymanLyesri/hyprland-conf
 fi
-
-cd ../nvim-config
-
-if ! git config remote.upstream.url > /dev/null; then
-    git remote add upstream https://github.com/jdhao/nvim-config
-fi
